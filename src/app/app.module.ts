3fddc6b1b0db6ef2890b282495399d30ef7c68f0
Matus Jurika <matus.jurika@zymestic.sk>
2020-08-24 15:54:01 +0200

    Add transloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco/transloco-root.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, NoopAnimationsModule, environment.production ? [] : AkitaNgDevtools.forRoot()],
+	imports: [BrowserModule, NoopAnimationsModule, environment.production ? [] : AkitaNgDevtools.forRoot(), HttpClientModule, TranslocoRootModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
